4e4d1eccd6336823fb3e08dbab580bbc3ad0e46f
VarunCypherV <varunvk2004@gmail.com>
2023-10-06 20:52:16 +0530

    UI Nav Bar Fix : Allignment , Feature : Hover effect

--- src/main_page/navbar.jsx
+++ src/main_page/navbar.jsx
@@ -7,8 +7,8 @@ function NavBar() {
       setIsOpen(!isOpen);
     };
     return (
-        <nav className="w-full bg-[#0D1117] shadow">
-        <svg className="md:absolute md:top-0 md:left-0 w-80 h-80 md:z-0" viewBox="0 0 100 100">
+      <nav className="w-full bg-[#0D1117] shadow">
+      <svg className="md:absolute md:top-0 md:left-0 w-80 h-80 md:z-0" viewBox="0 0 100 100">
         <defs>
           <linearGradient id="gradient" gradientTransform="rotate(45)">
             <stop offset="50%" stopColor="#FF52A8" />
@@ -17,33 +17,35 @@ function NavBar() {
         </defs>
         <ellipse cx="20" cy="20" rx="70" ry="70" fill="url(#gradient)" />
       </svg>
-            <div className="justify-end px-2 mx-auto md:items-center md:flex md:px-8 font-inter">
-                <div className="mt-5">
-                        <ul className="items-end justify-end ml-0 space-y-8 md:flex md:space-x-6 md:space-y-0">
-                            <li className="text-white hover:text-purple-500 underline-offset hover:no-underline ">
-                                <a href="#">Home</a>
-                            </li>
-                            <li className="text-white hover:text-purple-500 underline-offset hover:no-underline">
-                                <a href="#">About Us</a>
-                            </li>
-                            <li className="text-white hover:text-purple-500 underline-offset hover:no-underline">
-                                <a href="#">Blog</a>
-                            </li>
-                            <li className="text-white hover:text-purple-500 ml-0 underline-offset hover:no-underline">
-                            <span onMouseEnter={toggleMenu} className="text-white cursor-pointer hover:text-purple-500 hover:no-underline">Your Account</span>
-      {isOpen && (
-        <div className="absolute left-0 mt-2 w-48 bg-opacity-50 border border-gray-300 rounded shadow-lg">
-          <ul>
-            <li className="py-2 px-4 hover:bg-purple-500 hover:text-white cursor-pointer">Log In</li>
-            <li className="py-2 px-4 hover:bg-purple-500 hover:text-white cursor-pointer">Sign In</li>
+      <div className="justify-end px-2 mx-auto md:items-center md:flex mr-12 font-inter ">
+        <div className="mt-10">
+          <ul className="items-end ml-0 space-y-14 md:flex md:space-x-14 md:space-y-0">
+            <li className="text-white hover:text-yellow-500 underline-offset hover:no-underline">
+              <a href="#">Home</a>
+            </li>
+            <li className="text-white hover:text-yellow-500 underline-offset hover:no-underline">
+              <a href="#">About Us</a>
+            </li>
+            <li className="text-white hover:text-yellow-500 underline-offset hover:no-underline">
+              <a href="#">Blog</a>
+            </li>
+            <li className="text-white hover:text-yellow-500 ml-0 underline-offset hover:no-underline">
+              <span onMouseEnter={toggleMenu} className="text-white cursor-pointer hover:text-yellow-500 hover:no-underline">Your Account</span>
+              {isOpen && (
+                <div className="absolute left-0 mt-2 w-48 bg-opacity-50 border border-gray-300 rounded shadow-lg">
+                  <ul>
+                    <li className="py-2 px-4 hover:bg-purple-500 hover:text-white cursor-pointer">Log In</li>
+                    <li className="py-2 px-4 hover:bg-purple-500 hover:text-white cursor-pointer">Sign In</li>
+                  </ul>
+                </div>
+              )}
+            </li>
           </ul>
         </div>
-      )}
-                            </li>
-                        </ul>
-                    </div>
-            </div>
-        </nav>
+      </div>
+    </nav>
+    
+
     );
 }
 export default NavBar;
