14f0ac09d45e4070ba81e3078ba2fd81c5d77e65
Vedika Gupta <107416261+vedika-gupta@users.noreply.github.com>
2023-10-07 23:33:08 +0530

    fixed the responsiveness in smaller devices

--- src/main_page/cards.jsx
+++ src/main_page/cards.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Card = ({ title, index }) => {
   const [ref, inView] = useInView({
@@ -21,8 +21,13 @@ const Card = ({ title, index }) => {
       className="rounded-lg overflow-hidden"
     >
       <div className="bg-black text-white p-16 custom-border-gradient rounded-2xl flex flex-col justify-between items-center">
-        <h2 className="flex justify-center items-center text-3xl font-semibold mb-10 ">{title}</h2>
-        <button onClick={() => console.log(`Clicked "Read More" on: ${title}`)} className="bg-white hover:bg-purple-500 text-black font-bold px-8 py-4 rounded-xl">
+        <h2 className="flex justify-center items-center text-3xl font-semibold mb-10 ">
+          {title}
+        </h2>
+        <button
+          onClick={() => console.log(`Clicked "Read More" on: ${title}`)}
+          className="bg-white hover:bg-purple-500 text-black font-bold px-8 py-4 rounded-xl"
+        >
           Read More
         </button>
       </div>
@@ -41,20 +46,20 @@ function Cards() {
   ];
 
   return (
-<div className="h-auto bg-[#0D1117] font-poppins">
-  <div className="text-white flex flex-col text-[70px] md:flex md:flex-row md:items-center md:gap-8 md:justify-center lg:text-[100px] ">
-    <span className="mt-5 font-extrabold ">INNOVATE.</span>
-    <span
-      className="mt-5 font-extrabold bg-gradient-to-b from-pink-500 to-blue-500 text-transparent bg-clip-text inset-0"
-      style={{
-        '--tw-gradient-from': '#F300F8',
-        '--tw-gradient-to': '#2EA1E2',
-      }}
-    >
-      DEVELOP.
-    </span>
-    <span className="mt-5 font-extrabold">ENGAGE</span>
-  </div>
+    <div className="h-auto bg-[#0D1117] font-poppins">
+      <div className="text-white flex flex-col justify-center items-center sm:text-[70px] text-[42px] md:flex md:flex-row md:items-center md:gap-8 md:justify-center lg:text-[100px] ">
+        <span className="mt-5 font-extrabold">INNOVATE.</span>
+        <span
+          className="mt-3  font-extrabold bg-gradient-to-b from-pink-500 to-blue-500 text-transparent bg-clip-text inset-0"
+          style={{
+            "--tw-gradient-from": "#F300F8",
+            "--tw-gradient-to": "#2EA1E2",
+          }}
+        >
+          DEVELOP.
+        </span>
+        <span className="mt-3 font-extrabold">ENGAGE</span>
+      </div>
       <div className=" flex justify-center items-center">
         <div className=" bg-gray-900 px-5 py-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 rounded-3xl gap-4">
           {cardTitles.map((title, index) => (
