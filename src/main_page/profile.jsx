4c4b25eb34388dee5ec2e9ea4b326b86fcc24859
Vedant Pandey <vedantpandey459@gmail.com>
2023-10-08 13:26:22 +0530

    profile changes

--- src/main_page/profile.jsx
+++ src/main_page/profile.jsx
@@ -3,12 +3,16 @@ import meow from './pics/meow.png';
 import backgroundImage1 from './pics/273400317-fe0a38ce-9504-41b0-b8d6-4ac6eb557998.png';
 
 function Profile(){
-    const [isOpen, setIsOpen] = useState(false);
-    const [isOpen1, setIsOpen1] = useState(false);
-
-
+    const [isOpen, setIsOpen] = useState(1);
+    
+    const handleClick1=(e)=>{
+        setIsOpen(1)
+    }
+    const handleClick2=(e)=>{
+        setIsOpen(0)
+    }
     return (
-        <div className='font-bold bg-black text-white h-screen' style={{backgroundImage:`url(${backgroundImage1})`}}>
+        <div className='font-bold bg-black text-white h-screen' style={{backgroundImage:`url(${backgroundImage1})`,backgroundSize:'cover',backgroundAttachment:'fixed'}}>
 
             <div class="relative pt-10">
                 <img src={meow} alt="Cover Photo" class="lg:w-1/2 md:w-4/5 sm:w-4/5 mx-auto rounded-3xl h-40 object-cover"/>   
@@ -26,15 +30,74 @@ function Profile(){
             <div className='flex justify-center mt-40'>
                 <div className='w-1/2 flex justify-start'>
                     <div className='grid'>
-                        <button>Recent Events</button>
-                        <div className='h-1 w-20 bg-orange-700'></div>
+                        <button onClick={handleClick1}>Recent Events</button>
+                        <div className={isOpen?'h-1 w-20 bg-orange-700':'h-1 w-20 bg-black'}></div>
                         </div>
                     <div className='grid'>
-                        <button className='ml-8'>Registered Events</button>
-                        <div className='h-1 w-28 ml-8 bg-orange-700'></div>
+                        <button className='ml-8' onClick={handleClick2} >Registered Events</button>
+                        <div className={isOpen?'h-1 w-28 ml-8 bg-black':'h-1 w-28 ml-8 bg-orange-700'}></div>
                     </div>
                 </div>
             </div>
+            {isOpen ? (
+                <div class="flex flex-wrap w-3/5 m-auto ">
+                <div class="max-w-sm rounded overflow-hidden shadow-lg bg-blue-700  m-auto mt-10">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Recent Events</div>
+                    <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-blue-700 mt-10  m-auto">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Recent Events</div>
+                    <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+            </div>
+            ):(
+                <div class="flex flex-wrap w-3/5 m-auto">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-green-500 m-auto mt-10">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Registered Events</div>
+                    <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-green-500 m-auto mt-10">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Registered Events</div>
+                    <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+            </div>
+            </div>)
+            }
 
         </div>
     )
